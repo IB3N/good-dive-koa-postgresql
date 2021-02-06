b86a993b84ad5f75eaec2ffe0fd750e115019f25
Ben Pearce <benpearce9@live.co.uk>
2021-02-06 18:41:14 +0000

    add reviews controller

---
+++ controllers/reviews.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const review = require('../models/reviews-model');
+
+exports.getReviews = async ctx => {
+  try {
+    const reviews = await findAll();
+    ctx.body = reviews;
+    ctx.status(200);
+  } catch (o_0) {
+    console.error(o_0);
+    ctx.status(500);
+  }
+};
+
+exports.postReview = async ctx => {
+  try {
+    await insertReview(ctx.request.body);
+    ctx.status(201);
+  } catch (o_0) {
+    console.error(o_0);
+    ctx.status(501);
+  }
+};
